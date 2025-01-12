e3f6899a71661944696746d098f03275c5df4358
craigtkhill <craigtkhill@users.noreply.github.com>
2025-01-12 12:25:20 +0000

    deploy: 90e1d0756f42c8c9facfe65de875d0477c28f0f2

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="why-write-this-blog.html">Introduction</a></li><li class="chapter-item expanded "><a href="rust/set-width-of-polars-columns.html"><strong aria-hidden="true">1.</strong> How to Set the Width of a Polars Column</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="zettelkasten/zettelkasten-introduction.html">Introduction</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString();
         if (current_page.endsWith("/")) {
